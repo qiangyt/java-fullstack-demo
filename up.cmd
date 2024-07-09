9c6c6cc753ee585fbcb2e217ce07bc7391cfbf7f
Yiting Qiang <qiangyt@wxcount.com>
2024-07-09 11:21:45 +0800

    go on refining

--- up.cmd
+++ up.cmd
@@ -1,4 +1,6 @@
 docker-compose up -d --build --remove-orphans
 @REM docker compose up --build -d --remove-orphans
 
+timeout /t 5 /nobreak
+
 start http://localhost:8000
--- up.sh
+++ up.sh
@@ -3,4 +3,6 @@
 docker-compose up -d --build --remove-orphans
 # docker compose up -d --build --remove-orphans
 
+sleep 5
+
 open http://localhost:8000
